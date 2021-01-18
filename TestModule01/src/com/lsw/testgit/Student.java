860a798efccb6fd5c27d695eddda723218d07022
linshaowei <626853099@qq.com>
2021-01-18 21:30:25 +0800

    增加get和set方法以及新增构造器

--- TestModule01/src/com/lsw/testgit/Student.java
+++ TestModule01/src/com/lsw/testgit/Student.java
@@ -3,4 +3,28 @@ package com.lsw.testgit;
 public class Student {
     private int sno;
     String name;
+
+    public Student() {
+    }
+
+    public Student(int sno, String name) {
+        this.sno = sno;
+        this.name = name;
+    }
+
+    public int getSno() {
+        return sno;
+    }
+
+    public void setSno(int sno) {
+        this.sno = sno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
